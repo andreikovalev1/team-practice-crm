92a62366d7142421f9a744397293c624a13c43bf
Kovalenko Valeria <VKavalenka@ibagroup.eu>
2026-03-06 14:54:47 +0300

    [Feature] Исправлен z-index, чтобы шапка блюрилась при открытии модалки

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const showSearch = pathname === "/" || pathname.includes('/') && pathname.includes('cvs')
 
   return (
-    <header className="sticky top-0 z-40 bg-white px-6 pt-4 pb-4 flex flex-col gap-4">
+    <header className="sticky top-0 z-4 bg-white px-6 pt-4 pb-4 flex flex-col gap-4">
       <Breadcrumbs />
 
         {showSearch && 
